3140b279729806235f5b7fef6d703463ae6be301
saraiva791 <saraivaandre791@gmail.com>
2025-11-07 21:09:45 -0300

    atulização pra agenda 14

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,7 +1,37 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suite Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+             new Quarto()
+            {
+                Descricao = "Suite  Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+              new Quarto()
+            {
+                Descricao = "Suite Single",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+            },
+              new Quarto()
+            {
+                Descricao = "Suite Crise",
+                ValorDiariaAdulto = 25,
+                ValorDiariaCrianca = 12.5
+            }
+        };
+
         public App()
         {
             InitializeComponent();
--- MauiAppHotel/Views/ContrataçãoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContrataçãoHospedagem.xaml.cs
@@ -2,12 +2,36 @@ namespace MauiAppHotel.Views;
 
 public partial class ContrataçãoHospedagem : ContentPage
 {
+    App PropriedadesApp;
+
 	public ContrataçãoHospedagem()
 	{
 		InitializeComponent();
-	}
+
+        PropriedadesApp = (App)Application.Current;
+
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+    }
+
+   
+
     private async void OnSobreClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Sobre());
+    }
+
+   
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new HospedagemContratada());
+
+        } catch (Exception ex)
+
+        {
+            DisplayAlert("ops", ex.Message, "OK");
+        }
     }
 }
