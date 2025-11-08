32bf9e0acc362c205b18eb3b62706917913f00b8
saraiva791 <saraivaandre791@gmail.com>
2025-11-08 09:18:32 -0300

    atualização agenda 14

--- MauiAppHotel/Views/ContrataçãoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContrataçãoHospedagem.xaml.cs
@@ -11,6 +11,12 @@ public partial class ContrataçãoHospedagem : ContentPage
         PropriedadesApp = (App)Application.Current;
 
         pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month+1, DateTime.Now.Day);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
     }
 
    
@@ -33,5 +39,16 @@ public partial class ContrataçãoHospedagem : ContentPage
         {
             DisplayAlert("ops", ex.Message, "OK");
         }
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
+
     }
 }
